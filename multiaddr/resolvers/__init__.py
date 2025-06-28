b39118fbcc29f088617402f3c2c65a3785a5cb48
acul71 <luca.pisani@birdo.net>
2025-06-28 02:10:26 +0200

    chore: removed repeated code class Resolver

--- multiaddr/resolvers/__init__.py
+++ multiaddr/resolvers/__init__.py
@@ -1,26 +1,6 @@
 """DNS resolution support for multiaddr."""
 
-from typing import TYPE_CHECKING
-from typing import Protocol as TypeProtocol
-
+from .base import Resolver
 from .dns import DNSResolver
 
 __all__ = ["DNSResolver", "Resolver"]
-
-if TYPE_CHECKING:
-    from ..multiaddr import Multiaddr
-
-
-class Resolver(TypeProtocol):
-    """Base protocol for multiaddr resolvers."""
-
-    async def resolve(self, ma: "Multiaddr") -> list["Multiaddr"]:
-        """Resolve a multiaddr that contains a resolvable protocol.
-
-        Args:
-            ma: The multiaddr to resolve
-
-        Returns:
-            A list of resolved multiaddrs
-        """
-        ...
